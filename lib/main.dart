177738f48f0c3e9fbf84f4264bed4212c6d80c45
haneengheas <haneengheas@gmail.com>
2021-10-18 11:22:56 +0200

    انا طهجت

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:librarydalel/screens/registration/log_in_screen.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
--- lib/screens/user/profile_screen/View.dart
+++ lib/screens/user/profile_screen/View.dart
@@ -2,6 +2,7 @@
 // ignore: file_names
 // ignore_for_file: file_names
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:librarydalel/constant/styles.dart';
 import 'package:librarydalel/screens/user/profile_screen/user_item.dart';
@@ -20,14 +21,36 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   final user = FirebaseFirestore.instance.collection('books');
+  final email = FirebaseAuth.instance.currentUser!.email;
+
+  getUserName() async {
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc((await FirebaseAuth.instance.currentUser!).uid)
+        .get()
+        .then((value) {
+      setState(() {
+        var _userName = value.data()!['username'];
+        print(_userName);
+        print('==========================');
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getUserName();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: FutureBuilder<QuerySnapshot>(
+        body: FutureBuilder<DocumentSnapshot>(
             future: FirebaseFirestore.instance
                 .collection('users')
-                .where('userid', isEqualTo: 'dIhsAbpgyWOkUy1rfJzyHMwaqt52')
+                .doc(( FirebaseAuth.instance.currentUser!).uid)
                 .get(),
             builder: (context, snapshot) {
               if (snapshot.hasData) {
@@ -49,7 +72,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     const SizedBox(height: 50),
                     const SizedBox(height: 30),
                     // UserItem(" : البريد الألكترونى  ",
-                    //     textContainer: snapshot.data!.docs['username']),
+                    //     textContainer: snapshot.data!['username'].toString()),
                     const SizedBox(height: 70),
                     EditButton("تعديل بياناتى", onTap: () {
                       Navigator.push(
@@ -59,6 +82,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     }),
                     const SizedBox(height: 20),
                     Buton("تسجيل خروج", onTap: () {
+                      print(snapshot.data!['username']);
                       // Navigator.pushReplacement(context,
                       //     MaterialPageRoute(builder: (context) => LogInScreen()));
                     }),
