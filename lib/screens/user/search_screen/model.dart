4bca9e6262da30a0264caee6024f03cd68000f1d
EbrahimMShaaban <EbrahimMShaaban>
2021-10-19 15:03:20 +0200

    يا فرحة متمت

--- lib/screens/user/search_screen/model.dart
+++ lib/screens/user/search_screen/model.dart
@@ -1,17 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DataModel {
-  final String? name ;
-  // authname, imgurl, rownum, colnum, type, id
+  final String? name, authname, imgurl, colnum, rownum, type, id;
+
+  //   ,
   DataModel(
       {this.name,
-      // this.authname,
-      // this.imgurl,
-      // this.id,
-      // this.colnum,
-      // this.rownum,
-      // this.type
-      });
+      this.authname,
+      this.imgurl,
+      this.colnum,
+      this.id,
+      this.rownum,
+      this.type});
+
 
   //Create a method to convert QuerySnapshot from Cloud Firestore to a list of objects of this DataModel
   //This function in essential to the working of FirestoreSearchScaffold
@@ -23,14 +24,13 @@ class DataModel {
       print(dataMap['bookname']);
       print('============================================');
       return DataModel(
-          name: dataMap['bookname'],
-          // authname: dataMap['authorname'],
-          // imgurl: dataMap['imageurl'],
-          // colnum: dataMap['columnnum'],
-          // id: dataMap['bookid'],
-          // rownum: dataMap['rownum'],
-          // type: dataMap['type'],
-
+        name: dataMap['bookname'],
+        authname: dataMap['authorname'],
+        imgurl: dataMap['imageurl'],
+        colnum: dataMap['columnnum'],
+        id: dataMap['bookid'].toString(),
+        rownum: dataMap['rownum'],
+        type: dataMap['type'],
       );
     }).toList();
   }
--- lib/screens/user/search_screen/search.dart
+++ lib/screens/user/search_screen/search.dart
@@ -1,18 +1,16 @@
 import 'package:firestore_search/firestore_search.dart';
 import 'package:flutter/material.dart';
 import 'package:librarydalel/constant/styles.dart';
-import 'package:librarydalel/screens/user/home_screen/book_details/view.dart';
+import 'package:librarydalel/screens/admin/book_details/view.dart';
 
 import 'model.dart';
 
 class Seerch extends StatelessWidget {
   @override
-
   Widget build(BuildContext context) {
     return FirestoreSearchScaffold(
       firestoreCollectionName: 'books',
       searchBy: 'bookname',
-
       appBarBackgroundColor: purple,
       scaffoldBody: const Center(child: Text('Firestore Search')),
       dataListFromSnapshot: DataModel().dataListFromSnapshot,
@@ -26,32 +24,45 @@ class Seerch extends StatelessWidget {
                 final DataModel data = dataList![index];
 
                 return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
                   child: Directionality(
                     textDirection: TextDirection.rtl,
                     child: Container(
                       height: sizeFromHeight(context, 7),
                       decoration: BoxDecoration(
-                          color: purple,
+                          color: white2,
                           borderRadius: BorderRadius.circular(20)),
                       child: Center(
                         child: ListTile(
-                          // onTap: (){
-                          //   Navigator.push(context, MaterialPageRoute(builder: (context)=>  BookDetails(rownum: data.rownum, type: data.type, icon: null, bookname: data.name, id: data.id, colnum:data.colnum, image: data.imgurl, authname: data.authname,)));
-                          // },
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => BookDetails(
+                                          rownum: data.rownum,
+                                          type: data.type,
+                                          icon: null,
+                                          bookname: data.name,
+                                          id: data.id,
+                                          colnum: data.colnum,
+                                          image: data.imgurl,
+                                          authname: data.authname,
+                                        )));
+                          },
                           title: Text(
                             '${data.name}',
-                            style: buttonStyle,
+                            style: labelStyle,
                           ),
                           subtitle: Text(
-                            '',
-                            style: hintStyle,
-                              // ${data.authname}
-                            ),
-                          // trailing: CircleAvatar(
-                          //   radius: 35,
-                          //   backgroundImage: NetworkImage('${data.imgurl}'),
-                          // ),
+                            '${data.authname}',
+                            style: labelStyle2,
+                            //
+                          ),
+                          trailing: CircleAvatar(
+                            radius: 35,
+                            backgroundImage: NetworkImage('${data.imgurl}'),
+                          ),
                         ),
                       ),
                     ),
