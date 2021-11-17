576588615b789c67c2af29a0ae91c62d2218a3c6
haneengheas <haneengheas@gmail.com>
2021-11-17 19:25:18 +0200

    finish

--- lib/screens/admin/book_list_screen/displaybook_item.dart
+++ lib/screens/admin/book_list_screen/displaybook_item.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_typing_uninitialized_variables
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:librarydalel/constant/styles.dart';
@@ -111,9 +112,11 @@ class _DisplaybookItemState extends State<DisplaybookItem> {
                           ds.reference.delete();
                         }
                       });
-
+                      await FirebaseStorage.instance
+                          .refFromURL(widget.image.data()['imageurl'])
+                          .delete();
                       Navigator.pop(context);
-                      AwesomeDialog(
+                      await AwesomeDialog(
                               context: context,
                               title: "هام",
                               body: const Text("تمت عملية الحذف بنجاح"),
